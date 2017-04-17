c2ee58ed8dbf4383ba81b387ba84d7b55394429c
benrau87 <benrau87@users.noreply.github.com>
2017-04-17 14:29:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -99,7 +99,7 @@ cd tools/
 ###Add Repos
 ##Mongodb
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
 error_check 'Mongodb added'
 
 ##Java
