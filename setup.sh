9c366e34b66f01b99dc5f2e5e775d01e24d71215
benrau87 <benrau87@users.noreply.github.com>
2017-04-18 10:07:07 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -333,6 +333,7 @@ fi
 print_status "${YELLOW}Adding Sudo Access to Rooter${NC}"
 echo "400    ens33" >> /etc/iproute2/rt_tables &>> $logfile
 echo "cuckoo rooter --sudo &" | tee -a /etc/rc.local &>> $logfile
+echo "VBoxManage hostonlyif create" | tee -a /etc/rc.local &>> $logfile
 #echo "401    eth0" >> /etc/iproute2/rt_tables &>> $logfile
 error_check "Command Added, please restart to finish installation"
 
