ba244a29e00d35bea1c73854ed6abfb79ce0f462
benrau87 <benrau87@users.noreply.github.com>
2017-04-18 10:32:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -333,7 +333,7 @@ fi
 ##Rooter
 print_status "${YELLOW}Adding Sudo Access to Rooter${NC}"
 echo "400    ens33" >> /etc/iproute2/rt_tables &>> $logfile
-echo "cuckoo rooter --sudo &" | tee -a /etc/rc.local &>> $logfile
+echo "/usr/local/bin/cuckoo rooter --sudo &" | tee -a /etc/rc.local &>> $logfile
 #echo "401    eth0" >> /etc/iproute2/rt_tables &>> $logfile
 error_check "Command Added, please restart to finish installation"
 
