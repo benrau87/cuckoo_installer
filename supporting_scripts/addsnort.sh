0e29fd8c14bc5fde02dddeaaf0f2864298dadea8
benrau87 <benrau87@users.noreply.github.com>
2017-04-18 12:16:20 -0500

    Update addsnort.sh

--- supporting_scripts/addsnort.sh
+++ supporting_scripts/addsnort.sh
@@ -9,21 +9,52 @@ cd snort*
 ./configure --enable-sourcefire && make && make install
 ldconfig
 ln -s /usr/local/bin/snort /usr/sbin/snort
-mkdir /etc/snort
+groupadd snort
+sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
+
+mkdir -p /etc/snort/rules/iplists
+mkdir -p /etc/snort/rules/iplists
 mkdir /etc/snort/preproc_rules
-mkdir /etc/snort/rules
-mkdir /var/log/snort
+mkdir /etc/snort/preproc_rules
 mkdir /usr/local/lib/snort_dynamicrules
-touch /etc/snort/rules/white_list.rules
-touch /etc/snort/rules/black_list.rules
+mkdir /usr/local/lib/snort_dynamicrules
+mkdir /etc/snort/so_rules
+mkdir /etc/snort/so_rules
+mkdir -p /var/log/snort/archived_logs
+mkdir -p /var/log/snort/archived_logs
+
+touch /etc/snort/rules/iplists/black_list.rules
+touch /etc/snort/rules/iplists/black_list.rules
+touch /etc/snort/rules/iplists/white_list.rules
+touch /etc/snort/rules/iplists/white_list.rules
 touch /etc/snort/rules/local.rules
-chmod -R 5775 /etc/snort/
-chmod -R 5775 /var/log/snort/
-chmod -R 5775 /usr/local/lib/snort
-chmod -R 5775 /usr/local/lib/snort_dynamicrules/
-cp -avr *.conf *.map *.dtd /etc/snort/
-cp -avr src/dynamic-preprocessors/build/usr/local/lib/snort_dynamicpreprocessor/*  /usr/local/lib/snort_dynamicpreprocessor/
+touch /etc/snort/rules/local.rules
+touch /etc/snort/sid-msg.map
+touch /etc/snort/sid-msg.map
+
+chmod -R 5775 /etc/snort
+chmod -R 5775 /var/log/snort
+chmod -R 5775 /var/log/snort
+chmod -R 5775 /usr/local/lib/snort_dynamicrules
+chmod -R 5775 /usr/local/lib/snort_dynamicrules
+chown -R snort:snort /etc/snort
+chown -R snort:snort /etc/snort
+chown -R snort:snort /var/log/snort
+chown -R snort:snort /var/log/snort
+chown -R snort:snort /usr/local/lib/snort_dynamicrules
+chown -R snort:snort /usr/local/lib/snort_dynamicrules
+
+cd snort_src/snort-*/etc/
+cp *.conf* /etc/snort
+cp *.map /etc/snort
+cp *.dtd /etc/snort
+cd ~/snort_src/snort-*/src/dynamic-preprocessors/build/usr/local/lib/snort_dynamicpreprocessor/
+cp * /usr/local/lib/snort_dynamicpreprocessor/
+
 sed -i "s/include \$RULE\_PATH/#include \$RULE\_PATH/" /etc/snort/snort.conf
+
+
+
 
 snort -T -i vboxnet0 -c /etc/snort/snort.conf
 
