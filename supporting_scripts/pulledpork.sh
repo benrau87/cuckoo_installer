5e6b40ed114fadbd4a7826d041719d30edfb12b1
benrau87 <benrau87@users.noreply.github.com>
2017-04-18 12:25:38 -0500

    Update pulledpork.sh

--- supporting_scripts/pulledpork.sh
+++ supporting_scripts/pulledpork.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+wget https://github.com/finchy/pulledpork/archive/patch-3.zip
+unzip patch-3.zip
+cd pulledpork-patch-3
+sudo cp pulledpork.pl /usr/local/bin/
+chmod +x /usr/local/bin/pulledpork.pl
+cp etc/*.conf /etc/snort/
+
+/usr/local/bin/pulledpork.pl -V
+
