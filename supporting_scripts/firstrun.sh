1a83047df1e2df3fb9e6fbc974cf6a9e3764fab6
benrau87 <benrau87@users.noreply.github.com>
2017-04-19 13:40:13 -0500

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp ~/.mitmproxy/mitmproxy-ca-cert.p12 ~/.cuckoo/analyzer/windows/bin/cert.p12
 cuckoo &
 sleep 5
+cp ~/.mitmproxy/mitmproxy-ca-cert.p12 ~/.cuckoo/analyzer/windows/bin/cert.p12
 cp ~/conf/* ~/.cuckoo/conf/
 cuckoo community
 
