6247920a80de3cf28236a58d9505980fa7d54fb8
benrau87 <benrau87@users.noreply.github.com>
2017-04-21 14:23:09 -0500

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -5,4 +5,15 @@ cuckoo community
 cp ~/.mitmproxy/mitmproxy-ca-cert.p12 ~/.cuckoo/analyzer/windows/bin/cert.p12
 cp ~/conf/* ~/.cuckoo/conf/
 
+cd ~/.cuckoo/yara/
+git clone https://github.com/yara-rules/rules.git &>> $logfile
+cp rules/**/*.yar ~/.cuckoo/yara/binaries/ &>> $logfile
+##Remove Android and none working rules for now
+rm ~/.cuckoo/yara/binaries/Android* 
+rm ~/.cuckoo/yara/binaries/vmdetect.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/antidebug_antivm.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/MALW_AdGholas.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/APT_Shamoon*.yar  &>> $logfile
+
+
 
