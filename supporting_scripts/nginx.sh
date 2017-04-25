5d14cddbd0ead0102e5117d35608bc559caf0dd5
benrau87 <benrau87@users.noreply.github.com>
2017-04-25 09:28:34 -0500

    Update nginx.sh

--- supporting_scripts/nginx.sh
+++ supporting_scripts/nginx.sh
@@ -134,7 +134,7 @@ server {
     auth_basic_user_file /etc/nginx/htpasswd;
 
     location / {
-        proxy_pass http://127.0.0.1:8001;
+        proxy_pass http://127.0.0.1:8000;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -191,7 +191,7 @@ server {
     client_max_body_size 101M;
 
     location / {
-        proxy_pass http://127.0.0.1:8001;
+        proxy_pass http://127.0.0.1:8000
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
