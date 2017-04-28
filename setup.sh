614e9ff7a320185a5a405e0b1621e8be63bde353
benrau87 <benrau87@users.noreply.github.com>
2017-04-28 18:44:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -387,8 +387,8 @@ error_check 'Permissions set'
 #iptables -A FORWARD -s 10.1.1.0/24 -j ACCEPT &>> $logfile
 #iptables -A FORWARD -s 10.1.1.0/24 -d 10.1.1.0/24 -j ACCEPT &>> $logfile
 #iptables -A FORWARD -j LOG &>> $logfile
-#echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward &>> $logfile
-#sysctl -w net.ipv4.ip_forward=1 &>> $logfile
+echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward &>> $logfile
+sysctl -w net.ipv4.ip_forward=1 &>> $logfile
 #print_status "${YELLOW}Preserving Iptables${NC}"
 #apt-get -qq install iptables-persistent -y &>> $logfile
 #error_check 'Persistent Iptable entries'
