c747fc7b39b7b50bbdc4d6611752dbf7eeb70dfc
benrau87 <benrau87@users.noreply.github.com>
2017-05-09 16:16:54 -0500

    Update update_signatures.sh

--- supporting_scripts/update_signatures.sh
+++ supporting_scripts/update_signatures.sh
@@ -1,1 +1,93 @@
 #!/bin/bash
+####################################################################################################################
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+gitdir=$PWD
+
+##Logging setup
+logfile=/var/log/update_cuckoo_sigs.log
+mkfifo ${logfile}.pipe
+tee < ${logfile}.pipe $logfile &
+exec &> ${logfile}.pipe
+rm ${logfile}.pipe
+
+##Functions
+function print_status ()
+{
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+
+function print_good ()
+{
+    echo -e "\x1B[01;32m[*]\x1B[0m $1"
+}
+
+function print_error ()
+{
+    echo -e "\x1B[01;31m[*]\x1B[0m $1"
+}
+
+function print_notification ()
+{
+	echo -e "\x1B[01;33m[*]\x1B[0m $1"
+}
+
+function error_check
+{
+
+if [ $? -eq 0 ]; then
+	print_good "$1 successfully."
+else
+	print_error "$1 failed. Please check $logfile for more details."
+exit 1
+fi
+
+}
+
+function install_packages()
+{
+
+apt-get update &>> $logfile && apt-get install -y --allow-unauthenticated ${@} &>> $logfile
+error_check 'Package installation completed'
+
+}
+
+function dir_check()
+{
+
+if [ ! -d $1 ]; then
+	print_notification "$1 does not exist. Creating.."
+	mkdir -p $1
+else
+	print_notification "$1 already exists. (No problem, We'll use it anyhow)"
+fi
+
+}
+########################################
+##BEGIN MAIN SCRIPT##
+#Pre checks: These are a couple of basic sanity checks the script does before proceeding.
+
+print_status "${YELLOW}Updating Yara...Please Wait${NC}"
+cd /home/cuckoo/.cuckoo/yara/rules/ &>> $logfile
+git pull 
+chown -R cuckoo:cuckoo /home/cuckoo/.cuckoo/yara/
+cp **/*.yar ~/.cuckoo/yara/binaries/ &>> $logfile
+##Remove Android and none working rules for now
+rm ~/.cuckoo/yara/binaries/Android* &>> $logfile
+rm ~/.cuckoo/yara/binaries/vmdetect.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/antidebug_antivm.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/MALW_AdGholas.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/APT_Shamoon*.yar  &>> $logfile
+rm ~/.cuckoo/yara/binaries/peid.yar  &>> $logfile
+print_status "${YELLOW}Updating Suricata...Please Wait${NC}"
+etupdate -V
+service suricata restart
+print_status "${YELLOW}Updating Snort...Please Wait${NC}"
+pulledpork.pl -c /etc/snort/pulledpork.conf
+service snort restart
