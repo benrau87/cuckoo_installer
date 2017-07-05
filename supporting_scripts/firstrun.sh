f3705d5a59d5dd5154f9ffa9444159f0aba3c0a0
benrau87 <benrau87@users.noreply.github.com>
2017-07-05 16:26:55 -0500

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -2,18 +2,18 @@
 cuckoo &
 sleep 20
 cuckoo community
-cd ~/.cuckoo/yara/
-git clone https://github.com/yara-rules/rules.git 
-cp rules/**/*.yar ~/.cuckoo/yara/binaries/
+#cd ~/.cuckoo/yara/
+#git clone https://github.com/yara-rules/rules.git 
+#cp rules/**/*.yar ~/.cuckoo/yara/binaries/
 #cp ~/.mitmproxy/mitmproxy-ca-cert.p12 ~/.cuckoo/analyzer/windows/bin/cert.p12
 cp ~/conf/* ~/.cuckoo/conf
 ##Remove Android and none working rules for now
-rm ~/.cuckoo/yara/binaries/Android* 
-rm ~/.cuckoo/yara/binaries/vmdetect.yar  
-rm ~/.cuckoo/yara/binaries/antidebug_antivm.yar  
-rm ~/.cuckoo/yara/binaries/MALW_AdGholas.yar  
-rm ~/.cuckoo/yara/binaries/APT_Shamoon*.yar  
-rm ~/.cuckoo/yara/binaries/peid.yar  
+#rm ~/.cuckoo/yara/binaries/Android* 
+#rm ~/.cuckoo/yara/binaries/vmdetect.yar  
+#rm ~/.cuckoo/yara/binaries/antidebug_antivm.yar  
+#rm ~/.cuckoo/yara/binaries/MALW_AdGholas.yar  
+#rm ~/.cuckoo/yara/binaries/APT_Shamoon*.yar  
+#rm ~/.cuckoo/yara/binaries/peid.yar  
 
 
 
