38723fe83d528bf35425884c85d1137dcd99fc49
benrau87 <benrau87@users.noreply.github.com>
2017-07-05 14:27:39 -0500

    Update start_cuckoo.sh

--- supporting_scripts/start_cuckoo.sh
+++ supporting_scripts/start_cuckoo.sh
@@ -2,21 +2,17 @@
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
-
+ON=$(ifconfig -a | grep -cs 'vboxnet0')
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
-
+#start routing
 echo 1 | tee -a /proc/sys/net/ipv4/ip_forward 
 sysctl -w net.ipv4.ip_forward=1 
-
 sleep 5
-
-cuckoo rooter &
-
-ON=$(ifconfig -a | grep -cs 'vboxnet0')
-
+xterm -hold -e cuckoo rooter &
+#start cuckoo
 if [[ $ON == 1 ]]
 then
   echo "Host only interface is up"
@@ -25,9 +21,8 @@ else
   VBoxManage hostonlyif create vboxnet0
   VBoxManage hostonlyif ipconfig vboxnet0 --ip 10.1.1.254
 fi
-
-su - steve -c 'cuckoo xterm -hold -e' &
-su - steve -c 'cuckoo xterm -hold -e cuckoo -d process auto' &
-su - steve -c 'cuckoo xterm -hold -e cuckoo web runserver 0.0.0.0:8000' &
+su - steve -c 'xterm -hold -e cuckoo' &
+su - steve -c 'xterm -hold -e cuckoo -d process auto' &
+su - steve -c 'xterm -hold -e cuckoo web runserver 0.0.0.0:8000' &
 
 
