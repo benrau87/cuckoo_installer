dc78bb9531282bf43fd1f0f176a68ba46a0110ab
benrau87 <benrau87@users.noreply.github.com>
2017-07-06 10:49:54 -0500

    Update moloch.sh

--- supporting_scripts/moloch.sh
+++ supporting_scripts/moloch.sh
@@ -1,176 +1,259 @@
-#!/bin/bash
-####################################################################################################################
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+!/bin/sh
+# Use this script to install OS dependencies, downloading and compile moloch dependencies, and compile moloch capture.
+
+# This script will
+# * use apt-get/yum to install OS dependancies
+# * download known working versions of moloch dependancies
+# * build them statically
+# * configure moloch-capture to use them
+# * build moloch-capture
+
+
+GLIB=2.52.2
+YARA=3.6.1
+GEOIP=1.6.11
+PCAP=1.8.1
+CURL=7.54.1
+LUA=5.3.4
+DAQ=2.0.6
+
+TDIR="/data/moloch"
+DOPFRING=0
+DODAQ=0
+DOCLEAN=0
+
+while :
+do
+  case $1 in
+  -p | --pf_ring | --pfring)
+    DOPFRING=1
+    shift
+    ;;
+  -d | --dir)
+    TDIR=$2
+    shift 2
+    ;;
+  --daq)
+    DODAQ=1
+    shift
+    ;;
+  --clean)
+    DOCLEAN=1
+    shift
+    ;;
+  -*)
+    echo "Unknown option '$1'"
+    exit 1
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+
+MAKE=make
+
+# Installing dependencies
+echo "MOLOCH: Installing Dependencies"
+if [ -f "/etc/redhat-release" ]; then
+  sudo yum -y install wget curl pcre pcre-devel pkgconfig flex bison gcc-c++ zlib-devel e2fsprogs-devel openssl-devel file-devel make gettext libuuid-devel perl-JSON bzip2-libs bzip2-devel perl-libwww-perl libpng-devel xz libffi-devel readline-devel libtool
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH - yum failed"
+    exit 1
+  fi
+fi
+
+if [ -f "/etc/debian_version" ]; then
+  sudo apt-get -y install wget curl libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev libssl-dev libreadline-dev libtool
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH - apt-get failed"
+    exit 1
+  fi
+fi
+
+if [ "$(uname)" = "FreeBSD" ]; then
+    sudo pkg_add -Fr wget curl pcre flex bison gettext e2fsprogs-libuuid glib gmake libexecinfo
+    MAKE=gmake
+fi
+
+
+
+
+echo "MOLOCH: Downloading and building static thirdparty libraries"
+if [ ! -d "thirdparty" ]; then
+  mkdir thirdparty
+fi
+cd thirdparty || exit
+
+TPWD=`pwd`
+
+# glib
+if [ "$(uname)" = "FreeBSD" ]; then
+  #Screw it, use whatever the OS has
+  WITHGLIB=" "
+else
+  WITHGLIB="--with-glib2=thirdparty/glib-$GLIB"
+  if [ ! -f "glib-$GLIB.tar.xz" ]; then
+    GLIBDIR=$(echo $GLIB | cut -d. -f 1-2)
+    wget "http://ftp.gnome.org/pub/gnome/sources/glib/$GLIBDIR/glib-$GLIB.tar.xz"
+  fi
+
+  if [ ! -f "glib-$GLIB/gio/.libs/libgio-2.0.a" ] || [ ! -f "glib-$GLIB/glib/.libs/libglib-2.0.a" ]; then
+    xzcat glib-$GLIB.tar.xz | tar xf -
+    (cd glib-$GLIB ; ./configure --disable-xattr --disable-shared --enable-static --disable-libelf --disable-selinux --disable-libmount --with-pcre=internal; $MAKE)
+    if [ $? -ne 0 ]; then
+      echo "MOLOCH: $MAKE failed"
+      exit 1
+    fi
+  else
+    echo "MOLOCH: Not rebuilding glib"
+  fi
+fi
+
+# yara
+if [ ! -f "yara/yara-$YARA.tar.gz" ]; then
+  mkdir -p yara
+  wget https://github.com/VirusTotal/yara/archive/v$YARA.tar.gz -O yara/yara-$YARA.tar.gz
+fi
+
+if [ ! -f "yara/yara-$YARA/libyara/.libs/libyara.a" ]; then
+  (cd yara ; tar zxf yara-$YARA.tar.gz)
+  (cd yara/yara-$YARA; ./bootstrap.sh ; ./configure --enable-static; $MAKE)
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH: $MAKE failed"
+    exit 1
+  fi
+else
+  echo "MOLOCH: Not rebuilding yara"
+fi
+
+# GeoIP
+if [ ! -f "GeoIP-$GEOIP.tar.gz" ]; then
+  wget https://github.com/maxmind/geoip-api-c/releases/download/v$GEOIP/GeoIP-$GEOIP.tar.gz
+fi
+
+if [ ! -f "GeoIP-$GEOIP/libGeoIP/.libs/libGeoIP.a" ]; then
+tar zxf GeoIP-$GEOIP.tar.gz
+
+# Crossing fingers, this is no longer needed
+# Not sure why this is required on some platforms
+#  if [ -f "/usr/bin/libtoolize" ]; then
+#    (cd GeoIP-$GEOIP ; libtoolize -f)
+#  fi
+
+  (cd GeoIP-$GEOIP ; ./configure --enable-static; $MAKE)
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH: $MAKE failed"
+    exit 1
+  fi
+else
+  echo "MOLOCH: Not rebuilding libGeoIP"
+fi
+
+# libpcap
+if [ ! -f "libpcap-$PCAP.tar.gz" ]; then
+  wget http://www.tcpdump.org/release/libpcap-$PCAP.tar.gz
+fi
+tar zxf libpcap-$PCAP.tar.gz
+if [ ! -f "libpcap-$PCAP/libpcap.a" ]; then
+  echo "MOLOCH: Building libpcap";
+  (cd libpcap-$PCAP; ./configure --disable-dbus --disable-usb --disable-canusb --disable-bluetooth --with-snf=no; $MAKE)
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH: $MAKE failed"
+    exit 1
+  fi
+else
+  echo "MOLOCH: NOT rebuilding libpcap";
+fi
+PCAPDIR=$TPWD/libpcap-$PCAP
+PCAPBUILD="--with-libpcap=$PCAPDIR"
+
+# curl
+if [ ! -f "curl-$CURL.tar.gz" ]; then
+  wget http://curl.haxx.se/download/curl-$CURL.tar.gz
+fi
+
+if [ ! -f "curl-$CURL/lib/.libs/libcurl.a" ]; then
+  tar zxf curl-$CURL.tar.gz
+  ( cd curl-$CURL; ./configure --disable-ldap --disable-ldaps --without-libidn --without-librtmp; $MAKE)
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH: $MAKE failed"
+    exit 1
+  fi
+else
+  echo "MOLOCH: Not rebuilding curl"
+fi
+
+# lua
+if [ ! -f "lua-$LUA.tar.gz" ]; then
+  wget https://www.lua.org/ftp/lua-$LUA.tar.gz
+fi
+
+if [ ! -f "lua-$LUA/src/liblua.a" ]; then
+  tar zxf lua-$LUA.tar.gz
+  ( cd lua-$LUA; make MYCFLAGS=-fPIC linux)
+  if [ $? -ne 0 ]; then
+    echo "MOLOCH: $MAKE failed"
+    exit 1
+  fi
+else
+  echo "MOLOCH: Not rebuilding lua"
+fi
+
+# daq
+if [ $DODAQ -eq 1 ]; then
+  if [ ! -f "daq-$DAQ.tar.gz" ]; then
+    wget https://www.snort.org/downloads/snort/daq-$DAQ.tar.gz
+  fi
+
+  if [ ! -f "daq-$DAQ/api/.libs/libdaq_static.a" ]; then
+    tar zxf daq-$DAQ.tar.gz
+    ( cd daq-$DAQ; ./configure --with-libpcap-includes=$TPWD/libpcap-$PCAP/ --with-libpcap-libraries=$TPWD/libpcap-$PCAP; make; sudo make install)
+    if [ $? -ne 0 ]; then
+      echo "MOLOCH: $MAKE failed"
+      exit 1
+    fi
+  else
+    echo "MOLOCH: Not rebuilding daq"
+  fi
+fi
+
+
+# Now build moloch
+echo "MOLOCH: Building capture"
+cd ..
+echo "./configure --prefix=$TDIR $PCAPBUILD --with-yara=thirdparty/yara/yara-$YARA --with-GeoIP=thirdparty/GeoIP-$GEOIP $WITHGLIB --with-curl=thirdparty/curl-$CURL --with-lua=thirdparty/lua-$LUA"
+./configure --prefix=$TDIR $PCAPBUILD --with-yara=thirdparty/yara/yara-$YARA --with-GeoIP=thirdparty/GeoIP-$GEOIP $WITHGLIB --with-curl=thirdparty/curl-$CURL --with-lua=thirdparty/lua-$LUA
+
+if [ $DOCLEAN -eq 1 ]; then
+    $MAKE clean
+fi
+
+$MAKE
+if [ $? -ne 0 ]; then
+  echo "MOLOCH: $MAKE failed"
   exit 1
 fi
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
-gitdir=$PWD
 
-##Logging setup
-logfile=/var/log/moloch_install.log
-mkfifo ${logfile}.pipe
-tee < ${logfile}.pipe $logfile &
-exec &> ${logfile}.pipe
-rm ${logfile}.pipe
-
-##Functions
-function print_status ()
-{
-    echo -e "\x1B[01;34m[*]\x1B[0m $1"
-}
-
-function print_good ()
-{
-    echo -e "\x1B[01;32m[*]\x1B[0m $1"
-}
-
-function print_error ()
-{
-    echo -e "\x1B[01;31m[*]\x1B[0m $1"
-}
-
-function print_notification ()
-{
-	echo -e "\x1B[01;33m[*]\x1B[0m $1"
-}
-
-function error_check
-{
-
-if [ $? -eq 0 ]; then
-	print_good "$1 successfully."
-else
-	print_error "$1 failed. Please check $logfile for more details."
-exit 1
+(cd capture/plugins/lua; $MAKE)
+if [ $DOPFRING -eq 1 ]; then
+    (cd capture/plugins/pfring; $MAKE)
 fi
 
-}
-
-function install_packages()
-{
-
-apt-get update &>> $logfile && apt-get -y dist-upgrade &>> $logfile && apt-get install -y --allow-unauthenticated ${@} &>> $logfile
-error_check 'Package installation completed'
-
-}
-
-function dir_check()
-{
-
-if [ ! -d $1 ]; then
-	print_notification "$1 does not exist. Creating.."
-	mkdir -p $1
-else
-	print_notification "$1 already exists. (No problem, We'll use it anyhow)"
+if [ $DODAQ -eq 1 ]; then
+    (cd capture/plugins/daq; $MAKE)
 fi
 
-}
-
-########################################
-##BEGIN MAIN SCRIPT##
-#Pre checks: These are a couple of basic sanity checks the script does before proceeding.
-
-echo -e "${YELLOW}We need to create a local Moloch admin and need a password, please type one.${NC}"
-read THEPASSWORD
-
-
-echo -e "${YELLOW}Removing any old Java sources, apt-get packages.${NC}"
-rm /var/lib/dpkg/info/oracle-java7-installer*  &>> $logfile
-rm /var/lib/dpkg/info/oracle-java8-installer*  &>> $logfile
-apt-get purge oracle-java7-installer -y &>> $logfile
-apt-get purge oracle-java8-installer -y &>> $logfile
-rm /etc/apt/sources.list.d/*java*  &>> $logfile
-dpkg -P oracle-java7-installer  &>> $logfile
-dpkg -P oracle-java8-installer  &>> $logfile
-apt-get -f install  &>> $logfile
-
-print_status "${YELLOW}Installing dependencies and updates${NC}"
-apt-get update &>> $logfile
-install_packages apt-transport-https python build-essential software-properties-common python-software-properties &>> $logfile
-##Java
-add-apt-repository ppa:webupd8team/java -y &>> $logfile
-##Elasticsearch
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - &>> $logfile
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list &>> $logfile
-apt-get update &>> $logfile
-install_packages libwww-perl libjson-perl ethtool
-error_check 'Dependencies updated'
-cd ~
-
-ES=5.4
-NODEJS=6.10.3
-INSTALL_DIR=$PWD
-
-
-##Pfring
-echo -n "Use pfring? ('yes' enables) [no] "
-read USEPFRING
-PFRING=""
-if [ -n "$USEPFRING" -a "x$USEPFRING" = "xyes" ]; then
-echo "MOLOCH - Using pfring - Make sure to install the kernel modules"
-sleep 1
-PFRING="--pfring"
+if [ -f "/opt/snf/lib/libsnf.so" ]; then
+    (cd capture/plugins/snf; $MAKE)
 fi
 
-##Java install for elasticsearch
-print_status "${YELLOW}Installing Java${NC}"
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections &>> $logfile
-apt-get install oracle-java8-installer -y &>> $logfile
-error_check 'Java Installed'
-
-## ElasticSearch
-print_status "${YELLOW}Downloading and installing Elasticsearch${NC}"
-if [ ! -f "elasticsearch-${ES}.tar.gz" ]; then
-apt-get install -y elasticsearch &>> $logfile
-error_check 'Elasticsearch Installed'
-print_status "${YELLOW}Setting up Elasticsearch${NC}"
-systemctl daemon-reload &>> $logfile
-systemctl enable elasticsearch.service &>> $logfile
-systemctl start elasticsearch.service &>> $logfile
-error_check 'Elasticsearch service setup'
+if [ -f "/usr/local/lib/libpfring.so" ]; then
+    (cd capture/plugins/pfring; $MAKE)
 fi
 
-# NodeJS
-#print_status "${YELLOW}Downloading and installing NodeJS${NC}"
-#if [ ! -f "node-v${NODEJS}.tar.gz" ]; then
-#wget http://nodejs.org/dist/v${NODEJS}/node-v${NODEJS}.tar.gz  &>> $logfile
-#fi
 
-#tar xfz node-v${NODEJS}.tar.gz   &>> $logfile
-#cd node-v${NODEJS}  &>> $logfile
-#./configure  &>> $logfile
-#make  &>> $logfile
-#make install  &>> $logfile
-#./configure --prefix=${TDIR}  &>> $logfile
-#make install  &>> $logfile
-#error_check 'NodeJS installed'
+echo "Now type 'sudo make install' and 'sudo make config'"
 
-
-##Moloch
-print_status "${YELLOW}Downloading and installing Moloch${NC}"
-wget https://files.molo.ch/builds/ubuntu-16.04/moloch_0.18.2-1_amd64.deb &>> $logfile
-dpkg -i moloch* &>> $logfile
-bash /data/moloch/bin/Configure 
-bash/ data/moloch/bin/moloch_add_user.sh admin "Admin User" $THEPASSWORD --admin &>> $logfile
-perl /data/moloch/db/db.pl http://localhost:9200 init &>> $logfile
-systemctl start molochcapture.service &>> $logfile
-service molochcapture start &>> $logfile
-systemctl start molochviewer.service &>> $logfile
-service molochviewer start &>> $logfile
-error_check 'Moloch Installed'
-
-echo -e "${YELLOW}Moloch installed successfully, navigate to $HOSTNAME:8005 to view.${NC}"
-#git clone https://github.com/aol/moloch.git
-
-#git clone https://github.com/benrau87/MolochSetup.git
-
-#cp MolochSetup/Moloch\ Script.sh ~/moloch/
-
-#cd moloch
-
-#bash Moloch\ Script.sh
-
+exit 0
