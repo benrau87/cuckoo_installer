3a746cb13de930846a29ee1b75f9d2bd3de78ac6
benrau87 <benrau87@users.noreply.github.com>
2017-07-07 12:58:06 -0500

    Update check_yara.sh

--- supporting_scripts/check_yara.sh
+++ supporting_scripts/check_yara.sh
@@ -34,11 +34,11 @@ counter=0
 for x in $(cat $rules_path/index.txt)
 do
   if [ $counter -lt 5 ]; then # we are under the limit
-    vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x >> $out_file &
+    vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x | tee -a $out_file &
     let $[counter++];
   else
     read x < $out_file # waiting for a process to finish
-    vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x >> $out_file &
+    vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x | tee -a $out_file &
    fi
 done
 cat $out_file > /dev/null # let all the background processes end
