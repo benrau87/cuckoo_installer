2af0cc7682d60cdc660ed293c0ff1f05adb7c4b4
benrau87 <benrau87@users.noreply.github.com>
2017-07-07 11:18:24 -0500

    Update check_yara.sh

--- supporting_scripts/check_yara.sh
+++ supporting_scripts/check_yara.sh
@@ -4,12 +4,14 @@ mkdir /home/cuckoo/.cuckoo/yara/test/allrules
 rules_path=/home/cuckoo/.cuckoo/yara/test/
 cd $rules_path
 git clone https://github.com/yara-rules/rules.git 
-cp **/*.yar cd $rules_path/allrules
-ls -d $rules_path/allrules/*.yar > index.txt
 
-for x in index.txt
+for y in $(ls -d $rules_path/rules/*/)
 do
-  vol.py -f /home/cuckoo/.cuckoo/storeage/analyses/12/memory.dmp
+  ls -d $y/*.yar | tee -a $rules_path/index.txt
+
+for x in $rules_path/index.txt
+do
+  vol.py -f /home/cuckoo/.cuckoo/storeage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x
  else
   echo "Error in rule $x"
 done
