b6ac76c3de26ef6432475d23db79327b8609c82c
benrau87 <benrau87@users.noreply.github.com>
2017-07-08 17:56:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -125,6 +125,10 @@ cd tools/
 
 ###Add Repos
 print_status "${YELLOW}Adding Repositories${NC}"
+##Ubuntu
+echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial multiverse" tee -a /etc/apt/sources.list  &>> $logfile
+echo "deb-src http://us.archive.ubuntu.com/ubuntu/ xenial multiverse" tee -a /etc/apt/sources.list  &>> $logfile
+
 ##Mongodb
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
