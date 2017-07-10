f2c042a6f199037d41bb09f089fa21261682558e
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 11:25:35 -0500

    Update call_volatility.sh

--- supporting_scripts/call_volatility.sh
+++ supporting_scripts/call_volatility.sh
@@ -2,7 +2,7 @@
 x=$1
 vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp \
      --profile=Win7SP1x64 yarascan \
-     --yara-file=$rules_path/allrules/$x \
+     --yara-file=/home/cuckoo/.cuckoo/yara/test/allrules/$x \
      --output=text \
-     --output-file=$out_dir/$x.log \
-     &>/home/$name/Desktop/error.txt
+     --output-file=/home/cuckoo/Desktop/yararesults/$x.log \
+     &>/home/cuckoo/Desktop/error.txt
