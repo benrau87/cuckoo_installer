ef911c4151a5e0da1b4401a07dbccfdeda366c21
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 18:27:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -245,6 +245,7 @@ if [ "$vtx" == "true" ]; then
 	print_status "${YELLOW}Setting up IRMA${NC}"
 	cd $gitdir
 	ansible-galaxy install -r ansible-requirements.yml &>> $logfile
+	ansible-galaxy install gdamjan.uwsgi
 	wget https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.deb?_ga=2.3815416.933420289.1499534277-1169717771.1499534277 &>> $logfile
 	dpkg -i vagrant* &>> $logfile
 	git clone https://github.com/quarkslab/irma &>> $logfile
