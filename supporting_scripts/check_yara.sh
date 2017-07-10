e915dc0fd7239eb0f273e75424ecdd1866300382
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 09:21:45 -0500

    Update check_yara.sh

--- supporting_scripts/check_yara.sh
+++ supporting_scripts/check_yara.sh
@@ -20,9 +20,10 @@ rules_path=/home/cuckoo/.cuckoo/yara/test/
 out_dir=/home/cuckoo/Desktop/yararesults/
 
 cd $rules_path
-#git clone https://github.com/yara-rules/rules.git 
+git clone https://github.com/yara-rules/rules.git 
 
 cp $rules_path/rules/**/*.yar $rules_path/allrules/
+rm $rules_path/allrules/Android*
 ls $rules_path/allrules/ > $rules_path/rules.txt
 
 count=(ps aux | grep vol.py | wc -l)
@@ -31,17 +32,11 @@ for x in $(cat $rules_path/rules.txt)
 do
   if [ $count -lt 5 ]; then # we are under the limit
      echo $x
-     vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x --output=text --output-file=$out_dir/$x.log > out 2>error /home/cuckoo/Desktop/error.txt  &
+     vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x --output=text --output-file=$out_dir/$x.log &>/home/cuckoo/Desktop/error.txt  
   else
     wait
    fi
 done
-
-cat /tmp/pipe > /dev/null # let all the background processes end
-rm /tmp/pipe # remove fifo
-
-
-
 
 cat /home/cuckoo/Desktop/error.txt | grep Cannot | cut -d"/" -f9 | cut -d"("  -f1 > badrules.txt
 
@@ -50,18 +45,3 @@ cat /home/cuckoo/Desktop/error.txt | grep Cannot | cut -d"/" -f9 | cut -d"("  -f
 
 
 
-#for x in $(cat $rules_path/index.txt)
-#do
-#  vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$x
-#done
-
-
-#for x in $(cat $rules_path/rules.txt)
-#do
-#     while [`jobs | wc -l` -ge 20]
-#     do
-#     sleep 1
-#     done
-#     touch $out_dir/$x.log
-#     xterm -hold -e vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x  &
-#done
