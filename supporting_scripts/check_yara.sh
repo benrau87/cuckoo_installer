ef5ce9b4bc2a196836929ce35bcecd33d6052554
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 09:46:33 -0500

    Update check_yara.sh

--- supporting_scripts/check_yara.sh
+++ supporting_scripts/check_yara.sh
@@ -35,8 +35,8 @@ do
      echo $x
      vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x --output=text --output-file=$out_dir/$x.log &>/home/$name/Desktop/error.txt & 
   else
-    wait
-   fi
+     wait
+  fi
 done
 
 #cat /home/$name/Desktop/error.txt | grep Cannot | cut -d"/" -f9 | cut -d"("  -f1 > /home/$name/Desktop/badrules.txt
