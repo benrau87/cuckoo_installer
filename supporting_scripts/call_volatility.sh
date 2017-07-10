859f131b1f86fa1190ae1ebf5f55034c3ef5f513
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 12:41:34 -0500

    Delete call_volatility.sh

--- supporting_scripts/call_volatility.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-x=$1
-vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=/home/cuckoo/.cuckoo/yara/test/allrules/$x --output=text --output-file=/home/cuckoo/Desktop/yararesults/$x.log &>/home/cuckoo/Desktop/error.txt
