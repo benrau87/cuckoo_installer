d328fb2951f6233a6fa23fda300eef3647bfa26e
benrau87 <benrau87@users.noreply.github.com>
2017-07-10 08:30:46 -0500

    Update check_yara.sh

--- supporting_scripts/check_yara.sh
+++ supporting_scripts/check_yara.sh
@@ -30,7 +30,7 @@ count=(ps aux | grep vol.py | wc -l)
 for x in $(cat $rules_path/rules.txt)
 do
   if [ $count -lt 5 ]; then # we are under the limit
-    { vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x --output=text --output-file=$out_dir/$x.log > out 2>error /home/cuckoo/Desktop/error.txt } &
+     vol.py -f /home/cuckoo/.cuckoo/storage/analyses/12/memory.dmp --profile=Win7SP1x64 yarascan --yara-file=$rules_path/allrules/$x --output=text --output-file=$out_dir/$x.log > out 2>error /home/cuckoo/Desktop/error.txt  &
   else
     wait
    fi
