8cc0e3798b9b4cd9a9b7faab506e939fa726fa3a
benrau87 <benrau87@users.noreply.github.com>
2017-07-11 08:15:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -239,25 +239,6 @@ systemctl start molochviewer.service &>> $logfile
 service molochviewer start &>> $logfile
 error_check 'Moloch Installed'
 
-##IRMA
-if [ "$vtx" == "true" ]; then
-	print_status "${YELLOW}Setting up IRMA${NC}"
-	cd $gitdir
-	#wget https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.deb?_ga=2.3815416.933420289.1499534277-1169717771.1499534277 &>> $logfile
-	#dpkg -i vagrant* &>> $logfile
-	git clone https://github.com/quarkslab/irma &>> $logfile
-	cd irma/ansible/ &>> $logfile
-	pip install -r requirements.txt
-	ansible-galaxy install -r ansible-requirements.yml --force &>> $logfile
-	print_status "${YELLOW}Setting up IRMA VM, this can take up to 30 mins.${NC}"
-	vagrant up &>> $logfile
-	error_check 'IRMA Installed'
-	echo "172.16.1.30    www.frontend.irma" | tee -a /etc/hosts &>> $logfile
-	error_check 'IRMA installed'
-else
-	print_status "${YELLOW}Skipping setup of IRMA due to hardware constraints${NC}"
-fi
-
 ##Yara
 print_status "${YELLOW}Downloading Yara${NC}"
 cd $gitdir
