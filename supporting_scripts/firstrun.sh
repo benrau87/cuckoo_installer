0c420a20be024f8cc43f66cfb32c9c50ee6e5436
benrau87 <benrau87@users.noreply.github.com>
2017-07-11 11:00:01 -0500

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+if [ ! -d ~/.cuckoo ]; then
 cuckoo &
 sleep 20
-cuckoo community
 cp ~/conf/* ~/.cuckoo/conf
+else
+cuckoo community
+if [ ! -d ~/.cuckoo/yara/IOCs ]; then
 mkdir ~/.cuckoo/yara/IOCs/
 cd ~/.cuckoo/yara/IOCs/
+git clone https://github.com/Neo23x0/signature-base.git
+else
+cd ~/.cuckoo/yara/IOCs/
+rm -rf signature-base/
 git clone https://github.com/Neo23x0/signature-base.git
 
