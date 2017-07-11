9500bbfe5673eef3400e625c16109ebd1e701647
benrau87 <benrau87@users.noreply.github.com>
2017-07-11 08:13:35 -0500

    Create irma_install.sh

---
+++ supporting_scripts/irma_install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+####################################################################################################################
+
+#incorporate brad's signatures in to signatures/cross, remove andromedia/dridex_apis/chimera_api/deletes_self/cryptowall_apis
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+gitdir=$PWD
+
+##Logging setup
+logfile=/var/log/cuckoo_install.log
+mkfifo ${logfile}.pipe
+tee < ${logfile}.pipe $logfile &
+exec &> ${logfile}.pipe
+rm ${logfile}.pipe
+
+##Functions
+function print_status ()
+{
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+
+function print_good ()
+{
+    echo -e "\x1B[01;32m[*]\x1B[0m $1"
+}
+
+function print_error ()
+{
+    echo -e "\x1B[01;31m[*]\x1B[0m $1"
+}
+
+function print_notification ()
+{
+	echo -e "\x1B[01;33m[*]\x1B[0m $1"
+}
+
+function error_check
+{
+
+if [ $? -eq 0 ]; then
+	print_good "$1 successfully."
+else
+	print_error "$1 failed. Please check $logfile for more details."
+exit 1
+fi
+
+}
+
+function install_packages()
+{
+
+apt-get update &>> $logfile && apt-get install -y --allow-unauthenticated ${@} &>> $logfile
+error_check 'Package installation completed'
+
+}
+
+function dir_check()
+{
+
+if [ ! -d $1 ]; then
+	print_notification "$1 does not exist. Creating.."
+	mkdir -p $1
+else
+	print_notification "$1 already exists. (No problem, We'll use it anyhow)"
+fi
+
+}
+########################################
+##BEGIN MAIN SCRIPT##
+#Pre checks: These are a couple of basic sanity checks the script does before proceeding.
+##Depos add
+#this is a nice little hack I found in stack exchange to suppress messages during package installation.
+export DEBIAN_FRONTEND=noninteractive
