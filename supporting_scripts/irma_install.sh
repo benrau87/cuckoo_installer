08f0ceee5227ab84bec226c78e2f161c13fffb3a
benrau87 <benrau87@users.noreply.github.com>
2017-07-12 12:00:57 -0500

    Update irma_install.sh

--- supporting_scripts/irma_install.sh
+++ supporting_scripts/irma_install.sh
@@ -87,8 +87,6 @@ fi
 if [ "$vtx" == "true" ]; then
 	print_status "${YELLOW}Setting up IRMA${NC}"
 	cd $gitdir
-	#wget https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.deb?_ga=2.3815416.933420289.1499534277-1169717771.1499534277 &>> $logfile
-	#dpkg -i vagrant* &>> $logfile
 	git clone https://github.com/quarkslab/irma &>> $logfile
 	cd irma/ansible/ &>> $logfile
 	pip install -r requirements.txt
