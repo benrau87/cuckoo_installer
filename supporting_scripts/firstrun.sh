56a4a935271281d3e983e78cbea114f0026bd32d
benrau87 <benrau87@users.noreply.github.com>
2017-07-12 13:06:00 -0500

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -3,19 +3,23 @@ if [ ! -d ~/.cuckoo ]; then
 cuckoo &
 sleep 20
 cp ~/conf/* ~/.cuckoo/conf
+cuckoo community
 else
 cuckoo community
-if [ ! -d ~/.cuckoo/yara/IOCs ]; then
-mkdir ~/.cuckoo/yara/IOCs/
-cd ~/.cuckoo/yara/IOCs/
+
+if [ ! -d ~/.cuckoo/yara/custom ]; then
+mkdir ~/.cuckoo/yara/custom/
+cd ~/.cuckoo/yara/custom/
 git clone https://github.com/Neo23x0/signature-base.git
 else
-cd ~/.cuckoo/yara/IOCs/
+cd ~/.cuckoo/yara/custom/
 rm -rf signature-base/
 git clone https://github.com/Neo23x0/signature-base.git
-if [ ! -d ~/.cuckoo/yara/rules ]; then
+if [ ! -d ~/.cuckoo/yara/custom/rules ]; then
+cd ~/.cuckoo/yara/custom/
 git clone https://github.com/Yara-Rules/rules.git
 else
-rm -rf yara/
+rm -rf ~/.cuckoo/yara/custom/rules
+cd ~/.cuckoo/yara/custom/
 git clone https://github.com/Yara-Rules/rules.git
 fi
