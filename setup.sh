8ec8abc564db747379336aefd6da19817a6b0e87
benrau87 <benrau87@users.noreply.github.com>
2017-07-18 09:04:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -526,6 +526,19 @@ dir_check /usr/bin/cd-drive
 git clone https://github.com/benrau87/antivmdetect.git  &>> $logfile
 error_check 'Antivm tools downloaded'
 
+##TOR
+print_status "${YELLOW}Installing Tor..${NC}"
+apt-get update -y &>> $logfile
+echo "deb http://deb.torproject.org/torproject.org xenial main" |  sudo tee -a /etc/apt/sources.list &>> $logfile
+echo "deb-src http://deb.torproject.org/torproject.org xenial main" |  sudo tee -a /etc/apt/sources.list &>> $logfile
+gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 &>> $logfile
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - &>> $logfile
+apt-get update &>> $logfile
+apt-get install tor deb.torproject.org-keyring -y &>> $logfile
+echo "TransPort 10.1.1.254:9040" | tee -a /etc/tor/torrc
+echo "DNSPort 10.1.1.254:5353" | tee -a /etc/tor/torrc
+error_check 'Tor installed'
+
 ##Holding pattern for dpkg...
 print_status "${YELLOW}Waiting for dpkg process to free up...${NC}"
 print_status "${YELLOW}If this takes too long try running ${RED}sudo rm -f /var/lib/dpkg/lock${YELLOW} in another terminal window.${NC}"
