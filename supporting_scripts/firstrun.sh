a1d7abdf2d44c0505bf3e4369cbbadf1f761cfff
benrau87 <benrau87@users.noreply.github.com>
2018-01-05 10:27:35 -0600

    Update firstrun.sh

--- supporting_scripts/firstrun.sh
+++ supporting_scripts/firstrun.sh
@@ -9,21 +9,21 @@ else
 cuckoo community
 fi
 ##Other yara rules
-if [ ! -d ~/.cuckoo/yara/custom ]; then
-mkdir ~/.cuckoo/yara/custom/
-cd ~/.cuckoo/yara/custom/
-git clone https://github.com/Neo23x0/signature-base.git
-else
-cd ~/.cuckoo/yara/custom/
-rm -rf signature-base/
-git clone https://github.com/Neo23x0/signature-base.git
-fi
+#if [ ! -d ~/.cuckoo/yara/custom ]; then
+#mkdir ~/.cuckoo/yara/custom/
+#cd ~/.cuckoo/yara/custom/
+#git clone https://github.com/Neo23x0/signature-base.git
+#else
+#cd ~/.cuckoo/yara/custom/
+#rm -rf signature-base/
+#git clone https://github.com/Neo23x0/signature-base.git
+#fi
 ##More yara rules
-if [ ! -d ~/.cuckoo/yara/custom/rules ]; then
-cd ~/.cuckoo/yara/custom/
-git clone https://github.com/Yara-Rules/rules.git
-else
-rm -rf ~/.cuckoo/yara/custom/rules
-cd ~/.cuckoo/yara/custom/
-git clone https://github.com/Yara-Rules/rules.git
-fi
+#if [ ! -d ~/.cuckoo/yara/custom/rules ]; then
+#cd ~/.cuckoo/yara/custom/
+#git clone https://github.com/Yara-Rules/rules.git
+#else
+#rm -rf ~/.cuckoo/yara/custom/rules
+#cd ~/.cuckoo/yara/custom/
+#git clone https://github.com/Yara-Rules/rules.git
+#fi
