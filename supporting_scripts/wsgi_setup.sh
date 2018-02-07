3692ff74b230512c76f8c6d16a0660bac4d829d9
benrau87 <benrau87@users.noreply.github.com>
2018-02-07 11:25:52 -0600

    Update wsgi_setup.sh

--- supporting_scripts/wsgi_setup.sh
+++ supporting_scripts/wsgi_setup.sh
@@ -1,4 +1,84 @@
-##Webserver
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+exit 1
+fi
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+gitdir=$PWD
+
+##Logging setup
+logfile=/var/log/wsgi_install.log
+mkfifo ${logfile}.pipe
+tee < ${logfile}.pipe $logfile &
+exec &> ${logfile}.pipe
+rm ${logfile}.pipe
+
+##Functions
+function print_status ()
+{
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+
+function print_good ()
+{
+    echo -e "\x1B[01;32m[*]\x1B[0m $1"
+}
+
+function print_error ()
+{
+    echo -e "\x1B[01;31m[*]\x1B[0m $1"
+}
+
+function print_notification ()
+{
+   echo -e "\x1B[01;33m[*]\x1B[0m $1"
+}
+
+function error_check
+{
+
+if [ $? -eq 0 ]; then
+   print_good "$1 successfully."
+else
+   print_error "$1 failed. Please check $logfile for more details."
+exit 1
+fi
+
+}
+
+function install_packages()
+{
+
+apt-get update &>> $logfile && apt-get install -y --allow-unauthenticated ${@} &>> $logfile
+error_check 'Package installation completed'
+
+}
+
+function dir_check()
+{
+
+if [ ! -d $1 ]; then
+   print_notification "$1 does not exist. Creating.."
+   mkdir -p $1
+else
+   print_notification "$1 already exists. (No problem, We'll use it anyhow)"
+fi
+
+}
+
+############################################################################################################################
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+echo -e "${YELLOW}What is the name for the Cuckoo user on this machine?${NC}"
+read name
+
 print_status "${YELLOW}Configuring webserver${NC}"
 sudo adduser www-data $name  &>> $logfile
 sudo -i -u $name cuckoo web --uwsgi > /etc/uwsgi/apps-available/cuckoo-web.ini  &>> $logfile
