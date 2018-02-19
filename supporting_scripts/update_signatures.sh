c98a2b31fa0211def1c3257459462ccb99a605fd
benrau87 <benrau87@users.noreply.github.com>
2018-02-19 12:17:44 -0600

    Update update_signatures.sh

--- supporting_scripts/update_signatures.sh
+++ supporting_scripts/update_signatures.sh
@@ -78,17 +78,25 @@ cuckoo_yara=/home/cuckoo/.cuckoo/yara
 print_status "${YELLOW}Updating Cuckoo...Please Wait${NC}"
 su - cuckoo -c 'cuckoo community' 
 ##Critical Stack
-print_status "${YELLOW}Updating Critical Stack...Please Wait${NC}"
-critical-stack-intel pull 
-CS_RULES=/opt/critical-stack/frameworks/intel/master-public.bro.dat
-SURI_RULE_FILE=/etc/suricata/rules/cuckoo.rules
-BL2=/opt/bl2ru2/bl2ru2/bl2ru2.py
-rm $SURI_RULE_FILE
-touch $SURI_RULE_FILE
-
-awk '/ /{print $2 " " "Critical_Stack_Intel" " " $1}' $CS_RULES > /tmp/rules.list
-$BL2 /tmp/rules.list -o $SURI_RULE_FILE
-
+#print_status "${YELLOW}Updating Critical Stack...Please Wait${NC}"
+#critical-stack-intel pull 
+#CS_RULES=/opt/critical-stack/frameworks/intel/master-public.bro.dat
+#SURI_RULE_FILE=/etc/suricata/rules/cuckoo.rules
+#BL2=/opt/bl2ru2/bl2ru2/bl2ru2.py
+#rm $SURI_RULE_FILE
+#touch $SURI_RULE_FILE
+#awk '/ /{print $2 " " "Critical_Stack_Intel" " " $1}' $CS_RULES > /tmp/rules.list
+#$BL2 /tmp/rules.list -o $SURI_RULE_FILE
+##Update Yara sigsnatures
+print_status "${YELLOW}Updating Yara...Please Wait${NC}"
+cd /tmp
+git clone https://github.com/Yara-Rules/rules
+cd /rules
+cp CVE_Rules/* $cuckoo_yara/binaries
+cp Crypto/* $cuckoo_yara/binaries
+cp malware/* $cuckoo_yara/binaries
+cp Packers/* $cuckoo_yara/binaries
+cp $cuckoo_yara/binaries/* cp $cuckoo_yara/memory/* 
 ##Update IDS signatures
 print_status "${YELLOW}Updating Suricata...Please Wait${NC}"
 etupdate -V 
