2a7833c0517f7ffba316f584f6a9becc364dfcff
benrau87 <benrau87@users.noreply.github.com>
2018-02-21 17:11:21 -0600

    Update test_vmcloak.sh

--- supporting_scripts/test_vmcloak.sh
+++ supporting_scripts/test_vmcloak.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-033[0;31m'
+RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 gitdir=$PWD
 
 ##Logging setup
-logfile=/var/log/vmcloak_install.log
+logfile=~/vmcloak_install.log
 mkfifo ${logfile}.pipe
 tee < ${logfile}.pipe $logfile &
 exec &> ${logfile}.pipe
