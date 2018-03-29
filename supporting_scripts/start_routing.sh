882dc2717f6b7e998997838e1cf110beca13300d
benrau87 <benrau87@users.noreply.github.com>
2018-03-29 09:28:31 -0500

    Update start_routing.sh

--- supporting_scripts/start_routing.sh
+++ supporting_scripts/start_routing.sh
@@ -1,1 +1,94 @@
 
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+exit 1
+fi
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+gitdir=$PWD
+
+##Logging setup
+logfile=/var/log/vmcloak_install.log
+mkfifo ${logfile}.pipe
+tee < ${logfile}.pipe $logfile &
+exec &> ${logfile}.pipe
+rm ${logfile}.pipe
+
+##Functions
+function print_status ()
+{
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+
+function print_good ()
+{
+    echo -e "\x1B[01;32m[*]\x1B[0m $1"
+}
+
+function print_error ()
+{
+    echo -e "\x1B[01;31m[*]\x1B[0m $1"
+}
+
+function print_notification ()
+{
+	echo -e "\x1B[01;33m[*]\x1B[0m $1"
+}
+
+function error_check
+{
+
+if [ $? -eq 0 ]; then
+	print_good "$1 successfully."
+else
+	print_error "$1 failed. Please check $logfile for more details."
+exit 1
+fi
+
+}
+
+function install_packages()
+{
+
+apt-get update &>> $logfile && apt-get install -y --allow-unauthenticated ${@} &>> $logfile
+error_check 'Package installation completed'
+
+}
+
+function dir_check()
+{
+
+if [ ! -d $1 ]; then
+	print_notification "$1 does not exist. Creating.."
+	mkdir -p $1
+else
+	print_notification "$1 already exists. (No problem, We'll use it anyhow)"
+fi
+
+}
+
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
+
+echo -e "${RED}Active interfaces${NC}"
+for iface in $(ifconfig | cut -d ' ' -f1| tr '\n' ' ')
+do 
+  addr=$(ip -o -4 addr list $iface | awk '{print $4}' | cut -d/ -f1)
+  printf "$iface\t$addr\n"
+done
+echo -e "${YELLOW}What is the name of the interface which has an internet connection?(ex: eth0)${NC}"
+read interface
+
+if [ "$t1" = "$t2" ]; then
+  VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 &>> $logfile
+else
+  VBoxManage hostonlyif create &>> $logfile
+  VBoxManage hostonlyif ipconfig vboxnet0 --ip 192.168.56.1 &>> $logfile
+fi
+vmcloak-iptables 192.168.56.0/24 $interface
+error_check 'Interface configured, your Cuckoo VMs will now have internet access'
